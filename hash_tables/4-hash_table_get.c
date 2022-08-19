a3beff6cb7f9ad14f077757ef1125e0084430607
gallit0 <mateogesede@gmail.com>
2022-08-19 15:49:14 -0700

    task 4 finished

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get function
+ * @ht: hash table
+ * @key: key of slot to get
+ * Return: value
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int k;
+
+	if (!ht || !key)
+		return (0);
+	k = key_index(key, ht->size);
+	if (!(ht->array[k]))
+		return (0);
+	return (ht->array[k]->value);
+}
